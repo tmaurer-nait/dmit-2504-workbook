b80f28898ea1ea4c117774cb1a26f2faab2fabcd
Tom Maurer <tmaurer@nait.ca>
2024-09-06 10:31:56 -0600

    function problem 1 initialization

---
+++ Week1/day3/change_counter/bin/change_counter.dart
@@ -0,0 +1,5 @@
+import 'package:change_counter/change_counter.dart' as change_counter;
+
+void main(List<String> arguments) {
+  print('Hello world: ${change_counter.calculate()}!');
+}
---
+++ Week1/day3/change_counter/test/change_counter_test.dart
@@ -0,0 +1,8 @@
+import 'package:change_counter/change_counter.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
