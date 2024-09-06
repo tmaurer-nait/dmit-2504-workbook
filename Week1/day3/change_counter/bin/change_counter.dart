9ae9c810fa7b3859d72e26a90cd18d2a25da1ad9
Tom Maurer <tmaurer@nait.ca>
2024-09-06 11:13:09 -0600

    completed problem 1

--- Week1/day3/change_counter/bin/change_counter.dart
+++ Week1/day3/change_counter/bin/change_counter.dart
@@ -1,5 +1,9 @@
+import 'dart:io';
 import 'package:change_counter/change_counter.dart' as change_counter;
 
 void main(List<String> arguments) {
-  print('Hello world: ${change_counter.calculate()}!');
+  List<int> coinsInPocket = change_counter.promptForChange();
+  var total = change_counter.getTotal(coinCounts: coinsInPocket);
+
+  stdout.writeln('Your total change is worth: \$${total / 100}');
 }
--- Week1/day3/change_counter/lib/change_counter.dart
+++ Week1/day3/change_counter/lib/change_counter.dart
@@ -1,3 +1,48 @@
-int calculate() {
-  return 6 * 7;
+import 'dart:io';
+
+/// Prompt the user for change. [Returns] List of integers for each coin they have (pennies, nickels, dimes, quarters, loonies, toonies)
+///
+/// Throws an [Error] if user enters a negative value or if they enter a non integer value
+List<int> promptForChange() {
+  List<int> output = [];
+  List<String> coinTypes = [
+    'pennies',
+    'nickels',
+    'dimes',
+    'quarters',
+    'loonies',
+    'toonies'
+  ];
+
+  for (String coinType in coinTypes) {
+    stdout.writeln('How many $coinType do you have?');
+    late int coinCount;
+
+    try {
+      coinCount = int.parse(stdin.readLineSync()!);
+    } catch (e) {
+      throw Exception("User did not input an integer");
+    }
+
+    if (coinCount < 0) {
+      throw Exception("User entered negative value for coins");
+    }
+    output.add(coinCount);
+  }
+
+  return output;
+}
+
+/// [Returns] the total number of cents given a list of coins (pennies, nickels, dimes, quarters, loonies, toonies)
+int getTotal({required List<int> coinCounts}) {
+  int total = 0;
+
+  total += coinCounts[0];
+  total += coinCounts[1] * 5;
+  total += coinCounts[2] * 10;
+  total += coinCounts[3] * 25;
+  total += coinCounts[4] * 100;
+  total += coinCounts[5] * 200;
+
+  return total;
 }
