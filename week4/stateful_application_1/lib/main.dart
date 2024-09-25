b4376f893e88a4a447e6ba753a0f1f57c04a54cf
Tom Maurer <tmaurer@nait.ca>
2024-09-25 10:48:03 -0600

    Week 4 day 2 pt 1

--- week4/stateful_application_1/lib/main.dart
+++ week4/stateful_application_1/lib/main.dart
@@ -1,7 +1,6 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
+
+import 'package:stateful_application_1/widgets/random_dog.dart';
 
 void main() {
   runApp(const MainApp());
@@ -17,56 +16,5 @@ class MainApp extends StatelessWidget {
             body: Center(
       child: RandomDogImage(),
     )));
-  }
-}
-
-class RandomDogImage extends StatefulWidget {
-  const RandomDogImage({super.key});
-
-  @override
-  State<RandomDogImage> createState() => _RandomDogImageState();
-}
-
-class _RandomDogImageState extends State<RandomDogImage> {
-  String dogImageURL = '';
-
-  static Future<String> getRandomUrl() async {
-    const dogEndpoint = 'https://dog.ceo/api/breeds/image/random';
-    var response = await get(Uri.parse(dogEndpoint));
-    return await jsonDecode(response.body)["message"];
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    getRandomUrl().then((url) {
-      setState(() {
-        dogImageURL = url;
-      });
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        (dogImageURL.trim() == "")
-            ? const CircularProgressIndicator()
-            : Image.network(dogImageURL),
-        TextButton(
-            onPressed: () {
-              setState(() {
-                dogImageURL = '';
-              });
-              getRandomUrl().then((url) {
-                setState(() {
-                  dogImageURL = url;
-                });
-              });
-            },
-            child: const Text("Get A New Dog"))
-      ],
-    );
   }
 }
