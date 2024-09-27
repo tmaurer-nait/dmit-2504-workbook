4a7d5fda500180822ad961cb77725b4892c27354
Tom Maurer <tmaurer@nait.ca>
2024-09-27 11:55:42 -0600

    week 4 day 3 pt 2

--- week4/form_application/lib/main.dart
+++ week4/form_application/lib/main.dart
@@ -18,29 +18,93 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class UserSignupForm extends StatelessWidget {
+class UserSignupForm extends StatefulWidget {
   const UserSignupForm({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _UserSignupFormState();
+}
+
+class _UserSignupFormState extends State<UserSignupForm> {
+  // The key we can use to acces our forms current state
+  // This key uniquely identifes the form widget
+  final _formKey = GlobalKey<FormState>();
+
+  // create controllers to access the field values
+  final _userNameController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    // Clean up the controllers when the widget is disposed to free up memory
+    _userNameController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Form(
+      key: _formKey, // Assigning the key to our form
       child: Column(
         children: [
           const Text("New User Form"),
           TextFormField(
+            controller: _userNameController,
+            validator: (value) => value == null || value.trim() == ""
+                ? "userName cannot be empty"
+                : null,
             decoration: const InputDecoration(label: Text("username")),
-          ), // UserName Input
-          TextFormField(
-            obscureText: true,
-            enableSuggestions: false,
-            autocorrect: false,
-            decoration: const InputDecoration(label: Text("password")),
           ), // Password Input
+          PasswordFormField(controller: _passwordController),
           ElevatedButton(
-              onPressed: () => false, // Placeholder function
+              onPressed: () {
+                // Validate the input
+                if (_formKey.currentState!.validate()) {
+                  // If valid display something to show that the input is valid
+                  // Normally this is where we would submit the user data to a server
+                  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                      content: Text(
+                          "Username: ${_userNameController.text} Password: ${_passwordController.text}")));
+                }
+              }, // Placeholder function
               child: const Text("Sign Up")) // Signup Button
         ],
       ),
     );
   }
+}
+
+class PasswordFormField extends FormField<String> {
+  final TextEditingController controller;
+
+  PasswordFormField(
+      {Key? key,
+      FormFieldSetter<String>? onSaved,
+      FormFieldValidator<String>? validator,
+      required this.controller})
+      : super(
+            key: key,
+            onSaved: onSaved,
+            validator: validator,
+            builder: (state) {
+              return TextFormField(
+                controller: controller,
+                obscureText: true,
+                enableSuggestions: false,
+                autocorrect: false,
+                validator: (value) {
+                  if (value == null || value.trim() == "") {
+                    return "Password must not be empty";
+                  } else {
+                    return null;
+                  }
+                },
+                decoration: const InputDecoration(label: Text("password")),
+                // Required for correct validation of this field
+                onChanged: (value) {
+                  state.didChange(value);
+                },
+              );
+            });
 }
